7cef6e123372fac82c6b200835b930b6326f4bbb
praveer kumar <praveer.kumar@morressier.com>
2021-10-16 01:52:12 +0200

    Only technology samajhna

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use serde_json::{Result, Value};
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
 use std::env;
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
+use serde_json::{Result, Value};
+use indicatif::ProgressBar;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
     let file = File::open(filename)?;
@@ -11,12 +12,15 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> wher
 
 fn read_jsonl(filename: &str) -> Result<()> {
     if let Ok(lines) = read_lines(filename) {
+        let bar = ProgressBar::new(1000);
         for line in lines {
+            bar.inc(1);
             if let Ok(data) = line {
                 let v: Value = serde_json::from_str(&data)?;
                 println!("Please call {} at the number {}", v["name"], v["phone"]);
             }
         }
+        bar.finish();
     }
     Ok(())
 }
